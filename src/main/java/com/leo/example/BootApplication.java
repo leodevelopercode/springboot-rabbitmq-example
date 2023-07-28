b739e76a2de8e44e0bab1fdbece253252dd1c932
leo <nixgnixeix@gmail.com>
2023-07-28 19:57:48 +0800

    build framework.

---
+++ src/main/java/com/leo/example/BootApplication.java
@@ -0,0 +1,17 @@
+package com.leo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author leo thomas
+ * @ate 2023/7/28 下午 07:52
+ */
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class,args);
+    }
+    
+}
